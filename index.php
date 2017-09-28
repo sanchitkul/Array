df90109218241a57180cdcab07e0d3731f071077
Your Name <you@example.com>
2017-09-28 12:04:21 -0400

    Added 3 other functions

--- index.php
+++ index.php
@@ -4,17 +4,18 @@ $obj = new main();
 $numbers = array('1','2','3','4','5');
 $obj-> print_r($numbers);
 $obj-> array_product($numbers);
-$obj-> array_chunk($numbers);
+$obj-> array_slice($numbers);
 $obj-> array_sum($numbers);
 $obj-> rsort($numbers);
 $obj-> shuffle($numbers);
 $obj->  count($numbers);
 $obj-> array_rand($numbers);
+$obj-> array_pop($numbers); 
 class main
 {
 public function print_r($numbers)
 {
-echo '<pre>',print_r($numbers, true),'</pre>';
+echo '<h1><pre>',print_r($numbers, true),'</pre></h1>';
 echo'<hr>';
 }
 public function array_product($numbers)
@@ -23,10 +24,12 @@ echo'<h1> Calculating product of array</h1>';
 echo"Product of array:" . array_product($numbers);
 echo'<hr>';
 }
-public function array_chunk($numbers)
+public function array_slice($numbers)
 {
-echo'<h1>Splitting arrray</h1>';
-echo"Splitting of array : " . array_chunk($numbers,2);
+echo'<h1>Slicing an arrray</h1>';
+$pac=array_slice($numbers, 2);
+print_r($pac);
+
 echo'<hr>';
 }
 public function array_sum($numbers)
@@ -37,14 +40,16 @@ echo'<hr>';
 }
 public function rsort($numbers)
 {
-echo'<h1Resorting the Array</h1>';
-echo"Resorted array:" . rsort($numbers);
+echo'<h1>Resorting the Array</h1>';
+rsort($numbers);
+print_r($numbers);
 echo'<hr>';
 }
 public function shuffle($numbers)
 {
 echo'<h1>Shuffling the array</h1>';
-echo"Reschuffled array:" . shuffle($numbers);
+shuffle($numbers);
+print_r($numbers);
 echo'<hr>';
 }
 public function count($numbers)
@@ -59,19 +64,11 @@ echo'<h1>Picking random number from Array</h1>';
 echo"Random number selected:" . array_rand($numbers);
 echo'<hr>';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+public function array_pop($numbers)
+{
+echo '<h1> Poping of the elements </h1>';
+array_pop($numbers);
+print_r($numbers);
+}
 }
 ?>
